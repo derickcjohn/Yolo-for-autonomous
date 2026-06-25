832a9a3852e5f47c246b2372b9301fec52699354
Derick John <96041141+derickcjohn@users.noreply.github.com>
2026-06-25 12:38:11 +0530

    Switch to semantic segmentation model training
    Updated the model loading and training process to use a semantic segmentation model with new parameters.

--- train/training.py
+++ train/training.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
+# # Load a segmentation model
+# model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
 
-# Train the model 
-results = model.train(data="data.yaml", epochs=900, patience=100, imgsz=640)
+# # Train the model 
+# results = model.train(data="data.yaml", epochs=900, patience=100, imgsz=640)
+
+# Load semantic segmentation model
+model = YOLO("yolo26n-sem.pt")  # load a pretrained model (recommended for training)
+results = model.train(data="mapillary.yaml", epochs=900, patience=100, imgsz=1024, lr0=0.001)
